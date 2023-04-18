6fb708b2ab8ce5431f54d11267073419bf47de52
jeyjplugg <130372076+jeyjplugg@users.noreply.github.com>
2023-04-18 18:15:22 +0000

    Commit 1

--- src/Components/SecondaryButton.js
+++ src/Components/SecondaryButton.js
@@ -6,15 +6,21 @@ function SecondaryButton(props) {
     let minWidth = props.minWidth === undefined ? '144px' : props.minWidth;
     const Button = styled.div`
         background: #F2F2F2;
-        border: 1px solid #DFDFDF;
+        border: 1px solid #64748B;
+        border-radius: 24px;
         box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);
         min-width: ${minWidth};
-        color: #152842;
+        color: #64748B;
+        /* White */
+        background: #FFFFFF;
         &:hover {
-            background: linear-gradient(180deg, #DFEAF1 0%, #CFDDE8 100%);
+            background: #FFFFFF;
+            color: #64748B;
+            opacity: 0.8;
         }
         &:active {
             background: #DFDFDF;
+            opacity: 0.6;
         }
     `;
     return (
